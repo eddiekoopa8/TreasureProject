6015c49d2aa3d62f0916434b488dd9df05d4e6ba
Eddie Hill <550046@escg.ac.uk>
2025-09-23 13:44:51 +0100

    Main Menu

--- Assets/Scripts/TitleMan.cs
+++ Assets/Scripts/TitleMan.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.UIElements;
@@ -5,24 +6,42 @@ using UnityEngine.UIElements;
 public class TitleManager : MonoBehaviour
 {
     bool bShowDialog = false;
+    bool bShowMain = false;
+    bool bShowCredits = false;
+    bool bSentSignal = false;
 
     public enum SignalType {
-        SIGNAL_SELECT_NONE,
-        SIGNAL_SELECT_BTN_A,
-        SIGNAL_SELECT_BTN_B,
+        SIGNAL_NONE,              // Loop
+        SIGNAL_SELECT_DLG_BTN_A,  // Exit? Yes
+        SIGNAL_SELECT_DLG_BTN_B,  // Exit? No
+        SIGNAL_SELECT_MAIN_BTN_A, // Play
+        SIGNAL_SELECT_MAIN_BTN_B, // Options
+        SIGNAL_SELECT_MAIN_BTN_C, // Credits
+        SIGNAL_MAX,               // Shouldn't be used
     };
     SignalType signal;
+
 
     void Start()
     {
         bShowDialog = false;
+        bShowMain = false;
     }
 
     void Update()
     {
+        bool sentSignal = false;
+        if (bSentSignal)
+        {
+            Debug.Log("sent "+(int)signal);
+            sentSignal = true;
+        }
+
         if (Input.GetKeyDown(KeyCode.Return) || Input.GetKeyDown(KeyCode.KeypadEnter))
         {
-            ScnManager.Next();
+            //ScnManager.Next();
+            bShowMain = true;
+            bShowCredits = false;
         }
 
         if (Input.GetKeyDown(KeyCode.Escape) && !bShowDialog)
@@ -31,32 +50,58 @@ public class TitleManager : MonoBehaviour
         }
 
         ScnManager.GetUIObject("S_Dialog").SetActive(bShowDialog);
+        ScnManager.GetUIObject("MainMenu").SetActive(!bShowDialog && bShowMain && !bShowCredits);
+        ScnManager.GetUIObject("T_Start").SetActive(!bShowDialog && !bShowMain);
+        ScnManager.GetUIObject("T_Quit").SetActive(!bShowDialog);
+        ScnManager.GetUIObject("T_Credits").SetActive(!bShowDialog && bShowCredits);
 
         // signal handling
 
         switch (signal)
         {
-            case SignalType.SIGNAL_SELECT_NONE:
+            case SignalType.SIGNAL_NONE:
                 {
                     break;
                 }
-            case SignalType.SIGNAL_SELECT_BTN_A:
+            case SignalType.SIGNAL_SELECT_DLG_BTN_A:
                 {
                     ScnManager.Exit();
-                    signal = SignalType.SIGNAL_SELECT_NONE;
                     break;
                 }
-            case SignalType.SIGNAL_SELECT_BTN_B:
+            case SignalType.SIGNAL_SELECT_DLG_BTN_B:
                 {
                     bShowDialog = false;
-                    signal = SignalType.SIGNAL_SELECT_NONE;
                     break;
                 }
+            case SignalType.SIGNAL_SELECT_MAIN_BTN_A:
+                {
+                    ScnManager.Next();
+                    break;
+                }
+            case SignalType.SIGNAL_SELECT_MAIN_BTN_B:
+                {
+                    ScnManager.Next();
+                    break;
+                }
+            case SignalType.SIGNAL_SELECT_MAIN_BTN_C:
+                {
+                    bShowCredits = true;
+                    //ScnManager.Next();
+                    break;
+                }
+        }
+
+        if (sentSignal)
+        {
+            signal = SignalType.SIGNAL_NONE;
+            bSentSignal = false;
         }
     }
 
     public void SendSignal(int newSignal)
     {
+        Debug.Assert(signal > SignalType.SIGNAL_NONE || signal < SignalType.SIGNAL_MAX, "Bad Signal Parsed! ("+newSignal+")");
         signal = (SignalType)newSignal;
+        bSentSignal = true;
     }
 }
