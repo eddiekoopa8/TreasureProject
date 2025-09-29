53704f05409eed3ef527b8369b422d12e662d957
Eddie Hill <550046@escg.ac.uk>
2025-09-29 16:40:34 +0100

    ugh

---
+++ Assets/Scripts/PlayerMain.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class PlayerMain : BB_PhysicsObject
+{
+    
+    public override void ActorStart()
+    {
+        
+    }
+    public override void ActorUpdate()
+    {
+        
+    }
+}
