efddde9c438f3a4ece8f1d9cb1a069d6ef9e95a6
Eddie Hill <550046@escg.ac.uk>
2025-09-30 16:08:30 +0100

    Start player logic

--- Assets/Scripts/PlayerMain.cs
+++ Assets/Scripts/PlayerMain.cs
@@ -2,13 +2,22 @@ using UnityEngine;
 
 public class PlayerMain : BB_PhysicsObject
 {
-    
+    bool hitWall = false;
+    bool isHit = false;
     public override void ActorStart()
     {
         
     }
     public override void ActorUpdate()
     {
-        
+        hitWall = isLeft && isRight;
+        isHit = hitWall && isGrounded;
+
+        if (isGrounded && Input.GetKey(KeyCode.RightArrow))
+        {
+            rigidbody.linearVelocityX = 3;
+        }
+
+
     }
 }
